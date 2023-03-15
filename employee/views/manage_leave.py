388ba82b71614e9ff5c77c3667143fa81ee11f51
Sazzad Hossen <mshossen75@gmail.com>
2023-03-15 14:35:33 +0600

    modifid leave details in employee module

--- employee/views/manage_leave.py
+++ employee/views/manage_leave.py
@@ -1,3 +1,5 @@
+from django.db.models import Sum
+from datetime import datetime
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.shortcuts import redirect
@@ -15,6 +17,7 @@ from django.views.generic import UpdateView
 
 # Models
 from authority.models import LeaveApplication
+from authority.models import LatePresentAndLeave
 
 # Forms
 from authority.forms import LeaveApplicationForm
@@ -78,7 +81,14 @@ class LeaveDetailsView(LoginRequiredMixin, EmployeePassesTestMixin, DetailView):
     template_name = 'employee/leave_details.html'
 
     def get_context_data(self, **kwargs):
+        leave_object = self.object # Accessing the query object
+        leave_permonth = LatePresentAndLeave.objects.filter(is_active=True).order_by('-id').first()
+        total_leave_month = LeaveApplication.objects.filter(application_of=leave_object.application_of,leave_type=leave_object.leave_type, approved_status=True, leave_from__month=datetime.today().month).aggregate(total_days=Sum('total_days'))['total_days'] or 0
+        total_leave_year = LeaveApplication.objects.filter(application_of=leave_object.application_of,leave_type=leave_object.leave_type,approved_status=True, leave_from__year=datetime.today().year).aggregate(total_days=Sum('total_days'))['total_days'] or 0
         context = super().get_context_data(**kwargs)
         context["title"] = "Leave Details" 
+        context["leave_permonth"] = leave_permonth
+        context["total_leave_month"] = total_leave_month
+        context["total_leave_year"] = total_leave_year
         return context
     
